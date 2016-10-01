3c4bcf775957f131dd7bda8f7dd22bd53378497e
Turi <turi@Turis-MBP.nycap.rr.com>
2016-10-01 12:35:25 -0400

    Implemented keyboard controller directive
    Game now uses a reusable directive to setup keyboard controls

---
+++ node_modules/angularjs-color-picker/src/scripts/options-service.js
@@ -0,0 +1,37 @@
+export default class AngularColorPickerOptions {
+    constructor() {
+        return {
+            required: false,
+            disabled: false,
+            hue: true,
+            alpha: true,
+            round: false,
+            case: 'upper',
+            format: 'hsl',
+            pos: 'bottom left',
+            swatch: true,
+            swatchOnly: false,
+            swatchPos: 'left',
+            swatchBootstrap: true,
+            inline: false,
+            placeholder: '',
+            id: undefined,
+            name: undefined,
+            close: {
+                show: false,
+                label: 'Close',
+                class: '',
+            },
+            clear: {
+                show: false,
+                label: 'Clear',
+                class: '',
+            },
+            reset: {
+                show: false,
+                label: 'Reset',
+                class: '',
+            },
+        };
+    }
+}
